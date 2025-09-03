7a869d379f9d8cfef066e41c6531a99eb79f494f
Dante9l <zby99932@gmail.com>
2025-09-03 10:02:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Spring Boot 学习项目 README
+
+    这是一个用于学习与实践 Spring Boot 的示例项目。通过本项目你可以快速上手 Spring Boot 的基础用法、常见脚手架集成、测试与部署流程。
---
+++ src/test/java/top/zby/boot/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zby.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
